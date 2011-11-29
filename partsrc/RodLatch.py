1e7c787a9ec67d3142f4ffd37d3c8584397ef9f6
Clarence Risher <sparr0@gmail.com>
2011-11-28 21:29:08 -0500

    sync bolt hole spacing on y rod mounts and rod clamps, add comments

--- partsrc/RodLatch.py
+++ partsrc/RodLatch.py
@@ -11,36 +11,38 @@ except:
 	sys.path.append(path)
 
 z_rod_diameter = 8
-
-# import MendelMax.py settings here
-
+#how far apart should the bolt holes be?
+bolt_hole_spacing = 26
 #how thick should the non-flange parts be?
 latch_thickness = 7
 #how thick should the plastic under the bolt be?
 bolt_flange_thickness = 3
 #how big are the bolts?
+#TODO: add logic to specify bolt type instead of bolt dimensions
 bolt_hole_diameter = 5.5
 bolt_head_diameter = 8.5
 
+#TODO: import MendelMax.py settings here
+
 #bounding box for the main curved part of the latch
-box = Part.makeBox(26,14,z_rod_diameter/2+latch_thickness)
+box = Part.makeBox(bolt_hole_spacing,14,z_rod_diameter/2+latch_thickness)
 box.translate(Base.Vector(7,0,0))
 
 #main curved part of the latch, around the rod
 cylinder = Part.makeCylinder(z_rod_diameter/2+latch_thickness,14)
 cylinder.rotate(Base.Vector(0,0,0),Base.Vector(1,0,0),-90)
-cylinder.translate(Base.Vector(20,0,0))
+cylinder.translate(Base.Vector(7+bolt_hole_spacing/2,0,0))
 latch = box.common(cylinder)
 
 #connects the curved part to the bolt holes
-box = Part.makeBox(26,14,max(latch_thickness,bolt_flange_thickness))
+box = Part.makeBox(bolt_hole_spacing,14,max(latch_thickness,bolt_flange_thickness))
 box.translate(Base.Vector(7,0,0))
 latch = latch.fuse(box)
 
 #rod hole
 cylinder = Part.makeCylinder(z_rod_diameter/2,14)
 cylinder.rotate(Base.Vector(0,0,0),Base.Vector(1,0,0),-90)
-cylinder.translate(Base.Vector(20,0,0))
+cylinder.translate(Base.Vector(7+bolt_hole_spacing/2,0,0))
 latch = latch.cut(cylinder)
 
 #housings for the bolt holes
@@ -48,7 +50,7 @@ cylinder = Part.makeCylinder(7,latch_thickness)
 cylinder.translate(Base.Vector(7,7,0))
 latch = latch.fuse(cylinder)
 cylinder = Part.makeCylinder(7,latch_thickness)
-cylinder.translate(Base.Vector(33,7,0))
+cylinder.translate(Base.Vector(7+bolt_hole_spacing,7,0))
 latch = latch.fuse(cylinder)
 
 #bolt holes
@@ -59,10 +61,10 @@ cylinder = Part.makeCylinder(bolt_head_diameter/2,z_rod_diameter/2+latch_thickne
 cylinder.translate(Base.Vector(7,7,bolt_flange_thickness))
 latch = latch.cut(cylinder)
 cylinder = Part.makeCylinder(bolt_hole_diameter/2,z_rod_diameter/2+latch_thickness)
-cylinder.translate(Base.Vector(33,7,0))
+cylinder.translate(Base.Vector(7+bolt_hole_spacing,7,0))
 latch = latch.cut(cylinder)
 cylinder = Part.makeCylinder(bolt_head_diameter/2,z_rod_diameter/2+latch_thickness)
-cylinder.translate(Base.Vector(33,7,bolt_flange_thickness))
+cylinder.translate(Base.Vector(7+bolt_hole_spacing,7,bolt_flange_thickness))
 latch = latch.cut(cylinder)
 
 Part.show(latch)
