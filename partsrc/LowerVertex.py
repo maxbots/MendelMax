955fcf5735f79297279bf20667821ff31d82bb3c
Clarence Risher <sparr0@gmail.com>
2011-12-09 02:27:58 -0500

    Massive reorganization, parts as modules in a package, MendelMax.py as top level script

--- partsrc/LowerVertex.py
+++ partsrc/LowerVertex.py
@@ -1,68 +1,60 @@
 from __future__ import division # allows floating point division from integers
+import FreeCAD, Part, math
 from FreeCAD import Base
-from math import *
-import sys
-import os
 
-try:
-    path = os.path.join(os.path.dirname(__file__), '..', 'include')
-    i = sys.path.index(path)
-except:
-    sys.path.append(path)
-
-extrusion_profile = 20
-#how far apart should the bottom frame rectangles be?
-frame_rectangle_spacing = 30
-#how far apart should bolt holes into the same extrusion be?
-hole_spacing_medium = 20
-#how thick should the non-flange parts be?
+def LowerVertex(
+extrusion_profile = 20, 
+frame_rectangle_spacing = 30, 
+hole_spacing = 20, 
 thick_typical = 4.25
+):
 
-from MendelMax import *
+    #how big are the structural bolts?
+    #TODO: replace with use of bolt module after it's written
+    bolt_hole_diameter = 5.5
 
-#how big are the structural bolts?
-#TODO: replace with use of bolt module after it's written
-bolt_hole_diameter = 5.5
+    box = Part.makeBox(extrusion_profile*2+frame_rectangle_spacing+thick_typical,extrusion_profile*2,thick_typical)
+    box.translate(Base.Vector(-thick_typical,0,0))
+    vertex = box
 
-box = Part.makeBox(extrusion_profile*2+frame_rectangle_spacing+thick_typical,extrusion_profile*2,thick_typical)
-box.translate(Base.Vector(-thick_typical,0,0))
-vertex = box
+    #reinforce the base rectangle plate to the diagonal plate
+    filler_cross_section = Part.makePolygon([
+    Base.Vector(0,0,0),
+    Base.Vector(0,-(extrusion_profile*math.sqrt(3)-thick_typical)/math.sqrt(3),0),
+    Base.Vector(-(extrusion_profile*math.sqrt(3)-thick_typical),0,0),
+    Base.Vector(0,0,0),
+    ])
+    filler_face = Part.Face(filler_cross_section)
+    filler = filler_face.extrude(Base.Vector(0,0,thick_typical))
+    filler.translate(Base.Vector(-thick_typical,extrusion_profile*2,0))
+    vertex = vertex.fuse(filler)
 
-#reinforce the base rectangle plate to the diagonal plate
-filler_cross_section = Part.makePolygon([
-Base.Vector(0,0,0),
-Base.Vector(0,-(extrusion_profile*sqrt(3)-thick_typical)/sqrt(3),0),
-Base.Vector(-(extrusion_profile*sqrt(3)-thick_typical),0,0),
-Base.Vector(0,0,0),
-])
-filler_face = Part.Face(filler_cross_section)
-filler = filler_face.extrude(Base.Vector(0,0,thick_typical))
-filler.translate(Base.Vector(-thick_typical,extrusion_profile*2,0))
-vertex = vertex.fuse(filler)
+    #TODO: cleaner calculation of length of this part to reduce trimming
+    box = Part.makeBox(extrusion_profile+hole_spacing+40,extrusion_profile,thick_typical)
+    cylinder = Part.makeCylinder(bolt_hole_diameter/2,thick_typical)
+    cylinder.translate(Base.Vector(extrusion_profile/2,extrusion_profile/2,0))
+    box = box.cut(cylinder)
+    cylinder.translate(Base.Vector(hole_spacing,0,0))
+    box = box.cut(cylinder)
+    box.translate(Base.Vector(-(extrusion_profile+hole_spacing+15),0,0))
+    box.rotate(Base.Vector(0,extrusion_profile,0),Base.Vector(0,0,1),-30)
+    vertex = vertex.fuse(box)
+    #TODO: reduce or eliminate this trimming
+    box = Part.makeBox(40,10,extrusion_profile)
+    box.translate(Base.Vector(-thick_typical-5,-10,0))
+    vertex = vertex.cut(box)
 
-#TODO: cleaner calculation of length of this part to reduce trimming
-box = Part.makeBox(extrusion_profile+hole_spacing_medium+40,extrusion_profile,thick_typical)
-cylinder = Part.makeCylinder(bolt_hole_diameter/2,thick_typical)
-cylinder.translate(Base.Vector(extrusion_profile/2,extrusion_profile/2,0))
-box = box.cut(cylinder)
-cylinder.translate(Base.Vector(hole_spacing_medium,0,0))
-box = box.cut(cylinder)
-box.translate(Base.Vector(-(extrusion_profile+hole_spacing_medium+15),0,0))
-box.rotate(Base.Vector(0,extrusion_profile,0),Base.Vector(0,0,1),-30)
-vertex = vertex.fuse(box)
-#TODO: reduce or eliminate this trimming
-box = Part.makeBox(40,10,extrusion_profile)
-box.translate(Base.Vector(-thick_typical-5,-10,0))
-vertex = vertex.cut(box)
+    cylinder = Part.makeCylinder(bolt_hole_diameter/2,thick_typical)
+    cylinder.translate(Base.Vector(extrusion_profile/2,extrusion_profile/2,0))
+    vertex = vertex.cut(cylinder)
+    cylinder.translate(Base.Vector(0,extrusion_profile,0))
+    vertex = vertex.cut(cylinder)
+    cylinder.translate(Base.Vector(frame_rectangle_spacing+extrusion_profile,0,0))
+    vertex = vertex.cut(cylinder)
+    cylinder.translate(Base.Vector(0,-extrusion_profile,0))
+    vertex = vertex.cut(cylinder)
+    
+    return vertex
 
-cylinder = Part.makeCylinder(bolt_hole_diameter/2,thick_typical)
-cylinder.translate(Base.Vector(extrusion_profile/2,extrusion_profile/2,0))
-vertex = vertex.cut(cylinder)
-cylinder.translate(Base.Vector(0,extrusion_profile,0))
-vertex = vertex.cut(cylinder)
-cylinder.translate(Base.Vector(frame_rectangle_spacing+extrusion_profile,0,0))
-vertex = vertex.cut(cylinder)
-cylinder.translate(Base.Vector(0,-extrusion_profile,0))
-vertex = vertex.cut(cylinder)
-
-Part.show(vertex)
+if __name__ == "__main__":
+    Part.show(LowerVertex())
--- partsrc/YMotorMount.py
+++ partsrc/YMotorMount.py
@@ -1,104 +1,99 @@
 from __future__ import division # allows floating point division from integers
+import FreeCAD, Part, math
 from FreeCAD import Base
-import sys
-import os
 
-try:
-    path = os.path.join(os.path.dirname(__file__), '..', 'include')
-    i = sys.path.index(path)
-except:
-    sys.path.append(path)
-
-extrusion_profile = 20
+def YMotorMount(
+extrusion_profile = 20, 
 #how far apart should the bottom frame rectangles be?
-frame_rectangle_spacing = 30
-
-thick_typical = 4.25
+frame_rectangle_spacing = 30, 
+thick_typical = 4.25, 
 thick_compress = 3
+):
 
-from MendelMax import *
+    #TODO: use the bolt module
+    main_bolt_hole_diameter = 5.5
+    main_bolt_head_diameter = 10 #?
+    main_bolt_bore_depth = 4
 
-#TODO: use the bolt module
-main_bolt_hole_diameter = 5.5
-main_bolt_head_diameter = 10 #?
-main_bolt_bore_depth = 4
+    #TODO: use the motor and bolt modules
+    motor_bolt_hole_diameter = 3.5
+    motor_bolt_head_diameter = 6 #?
 
-#TODO: use the motor and bolt modules
-motor_bolt_hole_diameter = 3.5
-motor_bolt_head_diameter = 6 #?
+    mount_x = frame_rectangle_spacing+extrusion_profile*2
+    mount_y = extrusion_profile+thick_typical+6
+    base_z = thick_compress+main_bolt_bore_depth
 
-mount_x = frame_rectangle_spacing+extrusion_profile*2
-mount_y = extrusion_profile+thick_typical+6
-base_z = thick_compress+main_bolt_bore_depth
+    motor_plate_height = 16
 
-motor_plate_height = 16
+    #build the flat part of the mount
+    box = Part.makeBox(mount_x,mount_y,base_z)
+    mount = box
 
-#build the flat part of the mount
-box = Part.makeBox(mount_x,mount_y,base_z)
-mount = box
+    #TODO: use parametric motor dimensions
+    #TODO: waste less plastic on this plate
+    box = Part.makeBox(mount_x-motor_plate_height*2,thick_typical,motor_plate_height)
+    box.translate(Base.Vector(motor_plate_height,0,0))
+    plate = box
+    cylinder = Part.makeCylinder(motor_plate_height,thick_typical,Base.Vector(motor_plate_height,0,0),Base.Vector(0,1,0),90)
+    cylinder.rotate(Base.Vector(motor_plate_height,0,0),Base.Vector(0,1,0),-90)
+    plate = plate.fuse(cylinder)
+    cylinder.rotate(Base.Vector(mount_x/2,thick_typical/2,0),Base.Vector(0,0,1),180)
+    plate = plate.fuse(cylinder)
+    #motor bolt holes
+    cylinder = Part.makeCylinder(motor_bolt_hole_diameter/2,thick_typical)
+    cylinder.rotate(Base.Vector(0,0,0),Base.Vector(1,0,0),-90)
+    cylinder.translate(Base.Vector(mount_x/2-31/2,0,5.5))
+    plate = plate.cut(cylinder)
+    cylinder.translate(Base.Vector(31,0,0))
+    plate = plate.cut(cylinder)
+    cylinder.translate(Base.Vector(0,0,31))
+    plate = plate.cut(cylinder)
+    cylinder.translate(Base.Vector(-31,0,0))
+    plate = plate.cut(cylinder)
+    #motor cylinder hole
+    cylinder = Part.makeCylinder(23/2,thick_typical)
+    cylinder.rotate(Base.Vector(0,0,0),Base.Vector(1,0,0),-90)
+    cylinder.translate(Base.Vector(mount_x/2,0,motor_plate_height))
+    plate = plate.cut(cylinder)
+    plate.translate(Base.Vector(0,mount_y-thick_typical,base_z))
+    mount = mount.fuse(plate)
 
-#TODO: use parametric motor dimensions
-#TODO: waste less plastic on this plate
-box = Part.makeBox(mount_x-motor_plate_height*2,thick_typical,motor_plate_height)
-box.translate(Base.Vector(motor_plate_height,0,0))
-plate = box
-cylinder = Part.makeCylinder(motor_plate_height,thick_typical,Base.Vector(motor_plate_height,0,0),Base.Vector(0,1,0),90)
-cylinder.rotate(Base.Vector(motor_plate_height,0,0),Base.Vector(0,1,0),-90)
-plate = plate.fuse(cylinder)
-cylinder.rotate(Base.Vector(mount_x/2,thick_typical/2,0),Base.Vector(0,0,1),180)
-plate = plate.fuse(cylinder)
-#motor bolt holes
-cylinder = Part.makeCylinder(motor_bolt_hole_diameter/2,thick_typical)
-cylinder.rotate(Base.Vector(0,0,0),Base.Vector(1,0,0),-90)
-cylinder.translate(Base.Vector(mount_x/2-31/2,0,5.5))
-plate = plate.cut(cylinder)
-cylinder.translate(Base.Vector(31,0,0))
-plate = plate.cut(cylinder)
-cylinder.translate(Base.Vector(0,0,31))
-plate = plate.cut(cylinder)
-cylinder.translate(Base.Vector(-31,0,0))
-plate = plate.cut(cylinder)
-#motor cylinder hole
-cylinder = Part.makeCylinder(23/2,thick_typical)
-cylinder.rotate(Base.Vector(0,0,0),Base.Vector(1,0,0),-90)
-cylinder.translate(Base.Vector(mount_x/2,0,motor_plate_height))
-plate = plate.cut(cylinder)
-plate.translate(Base.Vector(0,mount_y-thick_typical,base_z))
-mount = mount.fuse(plate)
+    #extrusion bolt holes
+    cylinder = Part.makeCylinder(main_bolt_hole_diameter/2,base_z)
+    cylinder2 = Part.makeCylinder(main_bolt_head_diameter/2,main_bolt_bore_depth)
+    cylinder2.translate(Base.Vector(0,0,thick_compress))
+    cylinder = cylinder.fuse(cylinder2)
+    cylinder.translate(Base.Vector(extrusion_profile/2,(mount_y-thick_typical)/2,0))
+    mount = mount.cut(cylinder)
+    cylinder.translate(Base.Vector(mount_x-extrusion_profile,0,0))
+    mount = mount.cut(cylinder)
 
-#extrusion bolt holes
-cylinder = Part.makeCylinder(main_bolt_hole_diameter/2,base_z)
-cylinder2 = Part.makeCylinder(main_bolt_head_diameter/2,main_bolt_bore_depth)
-cylinder2.translate(Base.Vector(0,0,thick_compress))
-cylinder = cylinder.fuse(cylinder2)
-cylinder.translate(Base.Vector(extrusion_profile/2,(mount_y-thick_typical)/2,0))
-mount = mount.cut(cylinder)
-cylinder.translate(Base.Vector(mount_x-extrusion_profile,0,0))
-mount = mount.cut(cylinder)
+    #reinforcements
+    support_cross_section = Part.makePolygon([
+    Base.Vector(0,0,0),
+    Base.Vector(0,mount_y-thick_typical,0),
+    Base.Vector(0,mount_y-thick_typical,motor_plate_height*math.sin(math.acos((motor_plate_height-thick_typical)/motor_plate_height))),
+    Base.Vector(0,0,0)])
+    face = Part.Face(support_cross_section)
+    support = face.extrude(Base.Vector(thick_typical,0,0))
+    cylinder = Part.makeCylinder(motor_plate_height,mount_y,Base.Vector(motor_plate_height,0,0),Base.Vector(0,1,0),90)
+    cylinder.rotate(Base.Vector(motor_plate_height,0,0),Base.Vector(0,1,0),-90)
+    support1 = support.common(cylinder)
+    support1.translate(Base.Vector(0,0,base_z))
+    mount = mount.fuse(support1)
+    support.translate(Base.Vector(mount_x-thick_typical,0,0))
+    cylinder.rotate(Base.Vector(motor_plate_height,0,0),Base.Vector(0,1,0),90)
+    cylinder.translate(Base.Vector(mount_x-motor_plate_height*2))
+    support = support.common(cylinder)
+    support.translate(Base.Vector(0,0,base_z))
+    mount = mount.fuse(support)
 
-#reinforcements
-support_cross_section = Part.makePolygon([
-Base.Vector(0,0,0),
-Base.Vector(0,mount_y-thick_typical,0),
-Base.Vector(0,mount_y-thick_typical,motor_plate_height*math.sin(math.acos((motor_plate_height-thick_typical)/motor_plate_height))),
-Base.Vector(0,0,0)])
-face = Part.Face(support_cross_section)
-support = face.extrude(Base.Vector(thick_typical,0,0))
-cylinder = Part.makeCylinder(motor_plate_height,mount_y,Base.Vector(motor_plate_height,0,0),Base.Vector(0,1,0),90)
-cylinder.rotate(Base.Vector(motor_plate_height,0,0),Base.Vector(0,1,0),-90)
-support1 = support.common(cylinder)
-support1.translate(Base.Vector(0,0,base_z))
-mount = mount.fuse(support1)
-support.translate(Base.Vector(mount_x-thick_typical,0,0))
-cylinder.rotate(Base.Vector(motor_plate_height,0,0),Base.Vector(0,1,0),90)
-cylinder.translate(Base.Vector(mount_x-motor_plate_height*2))
-support = support.common(cylinder)
-support.translate(Base.Vector(0,0,base_z))
-mount = mount.fuse(support)
+    #zip-tie tab
+    box = Part.makeBox(10,10,base_z)
+    box.translate(Base.Vector((mount_x-10)/2,-10,0))
+    mount = mount.fuse(box)
+    
+    return mount
 
-#zip-tie tab
-box = Part.makeBox(10,10,base_z)
-box.translate(Base.Vector((mount_x-10)/2,-10,0))
-mount = mount.fuse(box)
-
-Part.show(mount)
+if __name__ == "__main__":
+    Part.show(YMotorMount())
